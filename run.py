0a33c9203c434a96e93c0436f04600fbb323e0b7
MartinaP87 <bleach1987@hotmail.it>
2022-07-15 21:03:14 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -12,15 +12,41 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("practise")
 
 
-bills = SHEET.worksheet("bills")
-data = bills.get_all_values()
-print(data)
-
-def choose_data_sheeet():
+def choose_data_sheet():
     """
     Allow the user to choose the field where the expence belongs to
     """
-    print("Please select what kind of expence you are updating today:\na: House bills,\nb: Car expences,\nc: Food expences,\nd: If you only want to view the total of your monthly expences.")
-    choice = input("Input a, b, c or d\n")
+    while True:
+        print("Please select what kind of expence you are updating today:\na: House bills,\nb: Car expences,\nc: Food expences,\nd: If you only want to view the total of your monthly expences.")
+        choice = input("Input a, b, c or d\n").lower()
 
-choose_data_sheeet()
+        if validate_input(choice):
+            print("Valid input")
+            break
+
+
+def validate_input(value):
+    """
+    Raise a value error if the value is not correct 
+    or if it's not exactly 1
+    """
+    try:
+        possible_choice = ("a", "b", "c", "d")
+        value in possible_choice
+        if len(value) != 1:
+            raise ValueError(
+                f"Only 1 value is required, you entered {len(value)} values"
+	        )
+        if value not in possible_choice:
+            raise ValueError(
+                f"You have input {value}; your value must be either a, b, c or d"
+                )
+    
+    except ValueError as e:
+        print(f"invalid data: {e}. Please try again.\n")
+        return False
+
+    return True
+
+
+choose_data_sheet()
