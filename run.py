29b66a75c8852e661f6a9b4f2f4496863c680738
MartinaP87 <bleach1987@hotmail.it>
2022-07-15 10:08:11 +0000

    Create choose data sheet function to allow the user to choose where to upload the new data or view the total expences

--- run.py
+++ run.py
@@ -12,6 +12,15 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("practise")
 
 
-foglio1 = SHEET.worksheet("foglio1")
-data = foglio1.get_all_values()
+bills = SHEET.worksheet("bills")
+data = bills.get_all_values()
 print(data)
+
+def choose_data_sheeet():
+    """
+    Allow the user to choose the field where the expence belongs to
+    """
+    print("Please select what kind of expence you are updating today:\na: House bills,\nb: Car expences,\nc: Food expences,\nd: If you only want to view the total of your monthly expences.")
+    choice = input("Input a, b, c or d\n")
+
+choose_data_sheeet()
